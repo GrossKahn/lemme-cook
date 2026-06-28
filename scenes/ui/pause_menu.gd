cb706195946548e91f70b43b33b7f9160dd82463
kathmik <ariesgirl2535@gmail.com>
2026-06-28 16:13:25 +0200

    Settings gefixt

--- scenes/ui/pause_menu.gd
+++ scenes/ui/pause_menu.gd
@@ -3,33 +3,32 @@ class_name PauseMenu extends Control
 @onready var grid_container: GridContainer = $GridContainer
 @onready var settings: Panel = $Settings
 
-var _is_paused:bool = false:
-	set = set_paused
-
-
 func _ready() -> void:
 	process_mode = Node.PROCESS_MODE_ALWAYS
-	
+	visible = get_tree().paused
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("pause"):
-		self._is_paused = !_is_paused
+		var new_state = not get_tree().paused
+		get_tree().paused = new_state
+		visible = new_state
 
 func set_paused(value:bool) -> void:
-	_is_paused = value
 	get_tree().paused = value
 	visible = value
 
 
 func _on_resume_btn_pressed() -> void:
-	_is_paused = false
+	get_tree().paused = false
+	visible = false
 
 
 func _on_settings_btn_pressed() -> void:
-	State.return_scene = "res://scenes/ui/pause_menu.tscn"
+	State.return_scene = "res://scenes/Main.tscn"
+	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/ui/settings.tscn")
 
 
 func _on_quit_btn_pressed() -> void:
-	get_tree().quit()
+	get_tree().change_scene_to_file("res://scenes/ui/menu.tscn")
 	
